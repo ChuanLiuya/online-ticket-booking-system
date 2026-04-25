d22f5cd870036ed0e8d98c4060973227c8a6643a
chuan_liu_ya <chuan_liu_ya@163.com>
2026-04-25 14:13:39 +0800

    feat: 创建users表

---
+++ server/src/modules/users/entities/users.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class Users {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 50, unique: true })
+  username: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  passwordHash: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', length: 20, nullable: true })
+  phone: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  nickname: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column({ type: 'datetime', nullable: true })
+  lastLoginAt: Date;
+}
