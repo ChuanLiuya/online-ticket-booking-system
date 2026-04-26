70db2e105f4d605cd71968b3052f6e50ecf4cfa6
chuan_liu_ya <chuan_liu_ya@163.com>
2026-04-26 08:35:39 +0800

    feat: 实现全局响应拦截器

---
+++ server/src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ApiResponseDto } from '../dto/api-response.dto';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<ApiResponseDto<any>> {
+    return next.handle().pipe(
+      map((data) => {
+        if (data instanceof ApiResponseDto) {
+          return data;
+        }
+        return new ApiResponseDto('操作成功', data);
+      }),
+    );
+  }
+}
