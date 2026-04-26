b1a87c056b5819376e3063e2fdd67b6efce2dcf8
chuan_liu_ya <chuan_liu_ya@163.com>
2026-04-26 19:18:25 +0800

    feat: 引入axios，对axios进行二次封装，并自定义错误类型

--- web/src/stores/user.ts
+++ web/src/stores/user.ts
@@ -7,16 +7,16 @@ import type { User } from '@/types/user'
 export const useUserStore = defineStore('user', () => {
   // 状态
   const user = ref<User | null>(null)
-  const accessToken = ref('')
+  const access_token = ref('')
 
   // 计算属性
-  const isLoggedIn = computed(() => user.value !== null && accessToken.value !== '')
+  const isLoggedIn = computed(() => user.value !== null && access_token.value !== '')
 
 
 
   return {
     user,
-    accessToken,
+    access_token,
     isLoggedIn,
   }
 })
