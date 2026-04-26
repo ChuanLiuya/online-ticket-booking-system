04851f79ee74cf63cdd2a7b29bcc2e6c2fc24d82
chuan_liu_ya <chuan_liu_ya@163.com>
2026-04-26 09:57:07 +0800

    feat: 设置包的依赖下载和导入全局

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,13 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TestModule } from './modules/test/test.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
