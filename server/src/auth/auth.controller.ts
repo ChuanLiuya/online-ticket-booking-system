6fee0df0c78910b0b43a86c3e3f6fe3bd747ab5d
chuan_liu_ya <chuan_liu_ya@163.com>
2026-04-26 08:44:04 +0800

    feat: 自定义响应message

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -2,13 +2,17 @@ import { Controller, Request, Post, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { User } from 'src/modules/users/entities/user.entity';
 import { AuthService } from './auth.service';
+import { ApiResponseDto } from 'src/common/dto/api-response.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Request() req: { user: User }) {
-    return this.authService.login(req.user);
+  login(
+    @Request() req: { user: User },
+  ): ApiResponseDto<{ access_token: string }> {
+    const data = this.authService.login(req.user);
+    return new ApiResponseDto('登录成功', data);
   }
 }
--- server/src/modules/users/users.controller.ts
+++ server/src/modules/users/users.controller.ts
@@ -3,6 +3,7 @@ import { UsersService } from './users.service';
 import { RegisterDto } from './dto/register.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { User } from './entities/user.entity';
+import { ApiResponseDto } from 'src/common/dto/api-response.dto';
 
 @Controller('users')
 export class UsersController {
@@ -10,13 +11,14 @@ export class UsersController {
 
   @Post()
   register(@Body() registerDto: RegisterDto) {
-    return this.usersService.register(registerDto);
+    const data = this.usersService.register(registerDto);
+    return new ApiResponseDto('注册成功', data);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get()
-  getCurrentUser(@Req() req: { user: User }): User {
+  getCurrentUser(@Req() req: { user: User }): ApiResponseDto<User> {
     const user = req.user;
-    return user;
+    return new ApiResponseDto('获取用户信息成功', user);
   }
 }
