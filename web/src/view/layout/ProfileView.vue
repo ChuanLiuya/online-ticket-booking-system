dda4ef1bb373d3e93fe308caf3d1d4339b13d115
chuan_liu_ya <chuan_liu_ya@163.com>
2026-04-27 10:06:51 +0800

    feat: profile界面

--- web/src/view/layout/ProfileView.vue
+++ web/src/view/layout/ProfileView.vue
@@ -1,16 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+
+const activeTab = ref(route.path)
+
+watch(activeTab, (val) => {
+  router.push(val)
+})
+
+watch(
+  () => route.path,
+  (val) => {
+    activeTab.value = val
+  },
+)
+</script>
 
 <template>
   <div class="container">
     <div>个人中心</div>
-    <RouterLink to="/profile/my-events">我的活动</RouterLink>
-    <RouterLink to="/profile/joined-events">已参加活动</RouterLink>
-    <RouterLink to="/profile/orders">我的订单</RouterLink>
-    <RouterLink to="/profile/settings">个人设置</RouterLink>
+    <el-tabs v-model="activeTab" class="profile-tabs">
+      <el-tab-pane label="我的活动" name="/profile/my-events" />
+      <el-tab-pane label="已参加活动" name="/profile/joined-events" />
+      <el-tab-pane label="我的订单" name="/profile/orders" />
+      <el-tab-pane label="个人设置" name="/profile/settings" />
+    </el-tabs>
     <RouterView />
   </div>
 </template>
 
 <style scoped>
-
+.profile-tabs {
+  margin-top: 20px;
+}
 </style>
