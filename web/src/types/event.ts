9fe32b7afd45d1b35e45e31b9ac89db792b2e083
chuan_liu_ya <chuan_liu_ya@163.com>
2026-04-27 18:15:42 +0800

    feat: 前端创建event类型

---
+++ web/src/types/event.ts
@@ -0,0 +1,37 @@
+import type { User } from "./user";
+
+export enum EventStatus {
+  UPCOMING = 'upcoming',
+  ONGOING = 'ongoing',
+  COMPLETED = 'completed',
+  CANCELED = 'canceled',
+}
+
+export interface Event {
+  //数据库字段
+  id: number;
+  title: string;
+  description: string;
+  location: string;
+  startTime: Date;
+  endTime: Date;
+  price: number;
+  maxParticipants: number;
+  currentParticipants: number;
+  status: EventStatus;
+  image: string;
+  //organizerId: string;
+
+  //关联用户
+  organizer: User;
+}
+
+export interface createEventReqBody {
+  title: string;
+  description: string;
+  location: string;
+  startTime: Date;
+  endTime: Date;
+  price: number;
+  maxParticipants: number;
+}
